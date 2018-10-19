7718fe0904f95f2f5cd9243e97c0e194eed8eda1
Alex Darby <alex.darby@w2globaldata.com>
2018-10-19 12:49:47 +0100

    Amendments to the references to JSON objects that have changed due to changing PD webhook.

--- web/pd2zendesk.php
+++ web/pd2zendesk.php
@@ -1,4 +1,10 @@
 <?php
+/*
+  TODO::
+    -impliment the annotate case.
+
+
+*/
 $messages = json_decode(file_get_contents("php://input"));
 
 $zd_subdomain = getenv('ZENDESK_SUBDOMAIN');
@@ -7,52 +13,98 @@ $zd_api_token = getenv('ZENDESK_API_TOKEN');
 $pd_api_token = getenv('PAGERDUTY_API_TOKEN');
 $pd_from_email = getenv('PAGERDUTY_USER_EMAIL');
 
+/*
+$zd_subdomain = "w2globaldatahelp";
+$zd_username = "alex.darby@w2globaldata.com";
+$zd_api_token = "92vBlkVMfsAsZbPJQaeRfAma3Ba5GZm0wOGZYezi";
+$pd_api_token = "ReaGHVfJN-T_qtLiCpP-";
+$pd_from_email = "alexjdarby@gmail.com";
+*/
 if ($messages) foreach ($messages->messages as $webhook) {
-  $webhook_type = $webhook->type;
-  $incident_id = $webhook->data->incident->id;
-  $ticket_id = $webhook->data->incident->trigger_summary_data->extracted_fields->ticket_id;
-  $ticket_url = $webhook->data->incident->html_url;
-  $pd_requester_id = $webhook->data->incident->assigned_to_user->id;
-
+  $webhook_type = $webhook->event;
+/*
+  echo "\$webhook_type = ";
+  var_dump($webhook_type);
+*/
+  $incident_id = $webhook->incident->incident_number;
+/*
+  echo "\$incident_id = ";
+  var_dump($incident_id);
+*/
+  $ticket_id = $webhook->log_entries[0]->id;
+/*
+  echo "\$ticket_id = ";
+  var_dump($ticket_id);
+*/
+  $ticket_url = $webhook->log_entries[0]->html_url;
+/*
+  echo "\$ticket_url = ";
+  var_dump($ticket_url);
+*/
+  $pd_requester_id = $webhook->incident->assignments[0]->assignee->summary;
+/*
+  echo "\$pd_requester_id = ";
+  var_dump($pd_requester_id);
+*/
   switch ($webhook_type) {
     case "incident.trigger":
       $verb = "triggered";
-      $assigned_array = $webhook->data->incident->assigned_to;
+      /*
+
+      
+      // Removed as the JSON object structure has changed and assignees are not processed in this way.
+
+      $assigned_array = $webhook->incident->assignments;
       $assigned_users = array();
       foreach ($assigned_array as $assigned_user) {
         array_push($assigned_users, $assigned_user->object->name);
       }
-      $action_message = " and is assigned to " . implode(", ", $assigned_users);
+      */
+      $action_message = " and is assigned to " . $pd_requester_id;
       //Remove the pd_integration tag in Zendesk to eliminate further updates
-      $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id/tags.json";
+      // $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id/tags.json";
+      $url = "http://httpresponder.com/sakjmdf9ef93";
       $data = array('tags'=>array('pd_integration'));
       $data_json = json_encode($data);
       $status_code = http_request($url, $data_json, "DELETE", "basic", $zd_username, $zd_api_token);
       break;
     case "incident.acknowledge":
       $verb = "acknowledged ";
+
+      /*
+
+      // Removed as the JSON object structure has changed and acknowledgers are not processed in this way. 
+
       $acknowledger_array = $webhook->data->incident->acknowledgers;
       $acknowledgers = array();
       foreach ($acknowledger_array as $acknowledger) {
         array_push($acknowledgers, $acknowledger->object->name);
       }
-      $action_message = " by " . implode(", ", $acknowledgers);
+      */
+      $acknowledger = $webhook->incident->acknowledgements[0]->acknowledger->summary;
+      $action_message = " by " . $acknowledger;
       break;
+    /*  
+    case "incident.annotate":
+      $verb = "annotated ";
+
+      $acknowledger = $webhook->incident->acknowledgements[0]->acknowledger->summary;
+      $action_message = " by " . $acknowledger;
+      break;
+    */
     case "incident.resolve":
       $verb = "resolved";
-      $action_message = " by " . $webhook->data->incident->resolved_by_user->name;
+      $action_message = " by " . $pd_requester_id;
       break;
     default:
       continue 2;
   }
   //Update the Zendesk ticket when the incident is acknowledged or resolved.
   $url = "https://$zd_subdomain.zendesk.com/api/v2/tickets/$ticket_id.json";
-
+  //$url = "http://mockbin.org/bin/be3f6d18-6693-45c3-9b5e-a516abc9022e";
   $data = array('ticket'=>array('comment'=>array('public'=>'false','body'=>"This ticket has been $verb" . $action_message . " in PagerDuty.  To view the incident, go to $ticket_url.")));
   $data_json = json_encode($data);
-
   $status_code = http_request($url, $data_json, "PUT", "basic", $zd_username, $zd_api_token);
-
   if ($status_code != "200" && $verb != "resolved") {
     //If we did not POST correctly to Zendesk, we'll add a note to the ticket, as long as it was a triggered or acknowledged ticket.
     $url = "https//api.pagerduty.com/incidents/$incident_id/notes";
@@ -62,6 +114,9 @@ if ($messages) foreach ($messages->messages as $webhook) {
   }
 }
 function http_request($url, $data_json, $method, $auth_type, $username, $token) {
+
+  // var_dump($url, $data_json, $method, $auth_type, $username, $token);
+
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   if ($auth_type == "token") {
@@ -83,7 +138,9 @@ function http_request($url, $data_json, $method, $auth_type, $username, $token) 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   $response  = curl_exec($ch);
   $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
   curl_close($ch);
   return $status_code;
 }
+
 ?>
